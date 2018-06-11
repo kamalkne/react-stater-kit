f62f39ddb8ebfa144c0ba76901bd85200f994b6b
Kamal Kokne <kamalkokne4@gmail.com>
2018-06-11 18:26:06 +0530

    Added Sass Support
    Added SASS loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,50 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-    module.exports = {
-      module: {
+/* === dont forget to import scss to main.js file === */
+/* ===> import './main.scss'; <=== */
+
+module.exports = {
+    module: {
         rules: [
-          {
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: {
-              loader: "babel-loader"
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }, {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true
+                        }
+                    }
+                ]
+            }, {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'style-loader' // creates style nodes from JS strings
+                    }, {
+                        loader: 'css-loader' // translates CSS into CommonJS
+                    }, {
+                        loader: 'sass-loader' // compiles Sass to CSS
+                    }
+                ]
             }
-          },
-	  {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            options: { minimize: true }
-          }
         ]
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
-      }
-        ]
-      },
-       plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
-    };
-
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        })
+    ]
+};
