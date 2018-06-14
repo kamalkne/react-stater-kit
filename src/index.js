aeb1ef8897d91e073da9f8b92611c62e34055a64
Kamal Kokne <kamalkokne4@gmail.com>
2018-06-14 17:17:40 +0530

    Updated landing JS

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-    import App from "./App";
-import style from "./main.css";
+import './main.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import asyncComponent from './asyncComponent';
+
+/* eslint-disable */
+const App = asyncComponent(() => {
+    return import('./App');
+});
+
+const Some = asyncComponent(() => {
+    return import('./Some');
+});
+/* eslint-enable */
+
+import {BrowserRouter, Route} from 'react-router-dom';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <div>
+            <Route exact path='/' component={App} />
+            <Route path='/some' component={Some} />
+        </div>
+    </BrowserRouter>,
+    document.getElementById('app')
+);
