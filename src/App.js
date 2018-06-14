edb6bb267cf716049738c314b66d995e31a19489
Kamal Kokne <kamalkokne4@gmail.com>
2018-06-14 17:16:18 +0530

    Updated App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-    import React from "react";
-    import ReactDOM from "react-dom";
-    const App = () => {
-      return (
-        <div>
-          <p>React here!</p>
-        </div>
-      );
-    };
-    export default App;
-    ReactDOM.render(<App />, document.getElementById("app"));
+import React from 'react';
+import {
+    Link
+} from 'react-router-dom';
+
+const App = () => (
+    <div>
+        <p> React here! </p>
+        <Link to="/some">Some</Link>
+    </div>
+);
+
+export default App;
+// ReactDOM.render( < App / > , document.getElementById('app'));
