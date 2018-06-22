e28822f3dafa4ed34b28b48a9125f916d79430ee
Kamal Kokne <kamalkokne4@gmail.com>
2018-06-22 16:27:35 +0530

    Added card style compoent

---
+++ src/styles/components/card.style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Card = styled.div`
+    height: 150px;
+    width: 250px;
+    padding: 40px;
+    border: 5px outset pink;
+`;
+
+export default Card;
