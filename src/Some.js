6f4befafa22c7573bb0831c517fffb7e57448810
Kamal Kokne <kamalkokne4@gmail.com>
2018-06-22 16:22:55 +0530

    Updated compoentn

--- src/Some.js
+++ src/Some.js
@@ -3,10 +3,15 @@ import {
     Link
 } from 'react-router-dom';
 
+import Card from './styles/components/card.style';
+import a from '../static/images/a.jpg';
+
 const Some = () => (
-    <div>
+    <div className="some">
         <p> Someone here! </p>
         <Link to="/">Home</Link>
+        <img src={a} alt="bohooo"/>
+        <Card></Card>
     </div>
 );
 
